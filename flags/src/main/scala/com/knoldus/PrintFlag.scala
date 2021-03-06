f1e9d666bd7a6a45d1a1973fb6a76dcad782ae1d
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-03-07 01:15:33 +0530

    Print, Help and Version flags

---
+++ flags/src/main/scala/com/knoldus/PrintFlag.scala
@@ -0,0 +1,7 @@
+package com.knoldus
+
+class PrintFlag {
+  def printFlag(message: String) = {
+    message.toUpperCase
+  }
+}
---
+++ flags/src/main/scala/com/knoldus/VersionFlag.scala
@@ -0,0 +1,7 @@
+package com.knoldus
+
+class VersionFlag {
+  def versionFlag() = {
+    "Version : 0.1"
+  }
+}
