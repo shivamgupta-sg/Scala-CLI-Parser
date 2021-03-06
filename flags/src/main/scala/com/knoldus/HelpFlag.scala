5c9b63cbbb3e52b168119e860b29ea5ee824869c
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-03-07 01:39:54 +0530

    Detailed Help Text

--- flags/src/main/scala/com/knoldus/HelpFlag.scala
+++ flags/src/main/scala/com/knoldus/HelpFlag.scala
@@ -3,6 +3,6 @@ package com.knoldus
 class HelpFlag {
   def helpFlag() = {
     val flagsMap = Map("-v" -> "show the version of program", "-h" -> "help", "-p" -> "print the message")
-    (flagsMap.map { case (k, v) => s" $k => $v " }).mkString("Usage scala [flag]\n", "\n", "")
+    (flagsMap.map { case (k, v) => s" $k => $v " }).mkString("Usage: cliparser options [message]\n", "\n", "")
   }
 }
